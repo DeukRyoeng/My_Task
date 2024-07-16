28c40746654a2d59651629c9bc7fb7670d28f864
Deuk Ryeong <164954344+DeukRyoeng@users.noreply.github.com>
2024-07-16 21:38:03 +0900

    싱글톤 패턴으로 데이터 전달

---
+++ Pokemon_Contact/Pokemon_Contact/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Pokemon_Contact
+//
+//  Created by DEUKRYEONG LEE on 7/16/24.
+//
+
+import Foundation
---
+++ Pokemon_Contact/Pokemon_Contact/Viewmodel.swift
@@ -0,0 +1,8 @@
+//
+//  Viewmodel.swift
+//  Pokemon_Contact
+//
+//  Created by DEUKRYEONG LEE on 7/16/24.
+//
+
+import Foundation
